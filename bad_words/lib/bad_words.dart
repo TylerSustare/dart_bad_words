eddf735044e5f464f3fd79c642a261621def2a02
Tyler Sustare <tylersutare@gmail.com>
2020-10-09 10:17:32 -0700

    WIP start on regex

--- bad_words/lib/bad_words.dart
+++ bad_words/lib/bad_words.dart
@@ -4,24 +4,48 @@ import 'word_list.dart';
 
 /// Bad Word Filter
 class Filter {
+  String replacer = '*';
+
   /// isProfane returns a boolean value representing if the string provided contains a profane word
-  bool isProfane(String stringToTest) {
-    final lowerCaseStringToTest = stringToTest.toLowerCase();
-    return wordList
-        .where((word) => lowerCaseStringToTest.contains(word))
-        .isNotEmpty;
+  bool isProfane(String string) {
+    final lowered = string.toLowerCase();
+    return wordList.where((word) => lowered.contains(word)).isNotEmpty;
   }
 
   /// replace tests a string, replacing bad words with an asterisk length string of equal length
-  String clean(String stringToObfuscate) {
-    final listToTest = stringToObfuscate.split(' ');
-    final clean = listToTest.map((e) {
-      if (wordSet.contains(e.toLowerCase())) {
-        return e.replaceAll(RegExp('.'), '*');
+  String clean(String string) {
+    wordList.forEach((badWord) {
+      try {
+        var badWordRegex = RegExp("($badWord)", caseSensitive: false);
+        if (string.contains(badWordRegex)) {
+          String replace = _replace(replacer: '*', word: badWord);
+          print('badWord');
+          print(badWord);
+          print('badWordRegex');
+          print(badWordRegex);
+          print('replace');
+          print(replace);
+          string = string.replaceAll(badWordRegex, replace);
+        }
+      } catch (e) {
+        print('*********************');
+        print(badWord);
+        print(e);
+        print('*********************');
       }
-      return e;
     });
+    return string;
+  }
 
-    return clean.join(' ');
+  String _replace({String word, String replacer}) {
+    String chars = '';
+    for (var i = 0; i < word.length; i++) {
+      if (word[i] == ' ') {
+        chars += ' ';
+      } else {
+        chars += replacer;
+      }
+    }
+    return chars;
   }
 }
--- bad_words/test/bad_words_test.dart
+++ bad_words/test/bad_words_test.dart
@@ -2,27 +2,36 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:bad_words/bad_words.dart';
 
 void main() {
-  test('tests isProfane method', () {
-    final filter = Filter();
-    expect(filter.isProfane('wholesome text'), false);
-    expect(filter.isProfane("All of the Dodgers can eat a 🍆"), true);
-    expect(filter.isProfane("Fuck the Dodgers"), true);
-    expect(filter.isProfane('All Dodgers are nobheads'), true);
-    expect(filter.isProfane('The Dodgers are sh!t'), true);
-    expect(() => filter.isProfane(null), throwsNoSuchMethodError);
+//   test('tests isProfane method', () {
+//     final filter = Filter();
+//     expect(filter.isProfane('wholesome text'), false);
+//     expect(filter.isProfane("All of the Dodgers can eat a 🍆"), true);
+//     expect(filter.isProfane("Fuck the Dodgers"), true);
+//     expect(filter.isProfane('All Dodgers are nobheads'), true);
+//     expect(filter.isProfane('The Dodgers are sh!t'), true);
+//     expect(() => filter.isProfane(null), throwsNoSuchMethodError);
+//   });
+
+//   test('test clean method', () {
+//     final filter = Filter();
+//     // clean string
+//     expect(filter.clean('test string without asterisks'),
+//         'test string without asterisks');
+//     // beginning of the string
+//     expect(filter.clean('fucking test string'), '****ing test string');
+
+//     // test more than one bad word
+//     expect(filter.clean('Those shitty Dodgers are arschloch'),
+//         'Those ****** Dodgers are *********');
+//   });
+
+  test('test exclamation in word', () {
+    // end of the string
+    expect(Filter().clean('All Dodgers are sh!t'), 'All Dodgers are ****');
   });
 
-  test('test clean method', () {
-    final filter = Filter();
-    // clean string
-    expect(filter.clean('test string without asterisks'),
-        'test string without asterisks');
-    // beginning of the string
-    expect(filter.clean('fucking test string'), '******* test string');
-    // end of the string
-    expect(filter.clean('All Dodgers are sh!t'), 'All Dodgers are ****');
-    // test more than one bad word
-    expect(filter.clean('Those shitty Dodgers are arschloch'),
-        'Those ****** Dodgers are *********');
-  });
+  // test('test clean method with spaces', () {
+  //   final filter = Filter();
+  //   expect(filter.clean('carpet muncher'), '**************');
+  // });
 }
